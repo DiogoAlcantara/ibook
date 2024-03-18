0638c015704575f5222253127614535f46f9912e
Diogo Alcantara <diogo.alcantara@solucx.com.br>
2024-03-18 19:08:08 -0300

    add: creating the skeleton to the project with nuxtjs

---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="presentation">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa vero
+      ipsam similique qui facere consequatur temporibus modi provident fugiat
+      ipsum animi, expedita iste libero, cumque natus voluptatibus fugit quos!
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa vero
+      ipsam similique qui facere consequatur temporibus modi provident fugiat
+      ipsum animi, expedita iste libero, cumque natus voluptatibus fugit quos!
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa vero
+      ipsam similique qui facere consequatur temporibus modi provident fugiat
+      ipsum animi, expedita iste libero, cumque natus voluptatibus fugit quos!
+    </p>
+    <img src="@/assets/img/presentation.svg" alt="Apresentação do iBook" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  p {
+    line-height: 26px;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
---
+++ components/templates/AboutTemplate.vue
@@ -0,0 +1,14 @@
+import PageTitle from '../atom/PageTitle.vue';
+<template>
+  <div class="home-template">
+    <PageTitle text="Bem vindo ao " strong="iBook" />
+    <Releases />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-template {
+  display: grid;
+  grid-gap: 4rem;
+}
+</style>
